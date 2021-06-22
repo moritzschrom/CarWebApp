8a50a61ff5b0d22d88537f5bbcfbf29daf859291
Moritz Schrom <moritz.schrom@schrom.co>
2021-06-22 08:24:42 +0200

    Initial commit.

---
+++ src/main/java/co/schrom/CarWebApp/CarWebAppApplication.java
@@ -0,0 +1,13 @@
+package co.schrom.CarWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/schrom/CarWebApp/CarWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package co.schrom.CarWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
